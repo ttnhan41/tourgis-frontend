819c88642659a6ae3ae275992b36a64a564e1490
Nguyen Ngoc Thanh Sang <nguyensang2032003@gmail.com>
2024-10-16 14:18:00 +0700

    add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,37 @@
 import { Link } from "react-router-dom";
-import '../assets/css/Login.css';
+// import '../assets/css/Login.css';
 import {FaUser, FaLock} from "react-icons/fa";
+import Wrapper from "../assets/wrappers/Login";
 
 const Login = () => {
   return (
-    <div className="wrapper">
-      {/* <h1>Login page</h1>
-      <Link to="/register">Register page</Link> */}
-      <form action="">
-        <h1>Log In</h1>
-        <div className="input-box">
-          <input type="text" placeholder="Username" required />
-          <FaUser className="icon"/>
-        </div>
+    <Wrapper>
+      <div className="wrapper">
+        <form action="">
+          <h1>Log In</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaUser className="icon"/>
+          </div>
 
-        <div className="input-box">
-          <input type="password" name="" id="" placeholder="Password" required />
-          <FaLock className="icon"/>
-        </div>
-        
+          <div className="input-box">
+            <input type="password" name="" id="" placeholder="Password" required />
+            <FaLock className="icon"/>
+          </div> 
 
-        <div className="remember-forgot">
-          <label><input type="checkbox" name="" id="" />Remember me</label>
-          <Link to="#">Forgot password?</Link>
-        </div>
+          <div className="remember-forgot">
+            <label><input type="checkbox" name="" id="" />Remember me</label>
+            <Link to="#" className="forgot">Forgot password?</Link>
+          </div>
 
-        <button type="submit">Login</button>
+          <button type="submit" className="btn">Login</button>
 
-        <div className="register-link">
-          <p>Don't have an account? <Link to="/register">Register</Link></p>
-        </div>
-      </form>
-    </div>
+          <div className="register-link">
+            <div>Don't have an account? <Link to="/register" className="register">Register</Link></div>
+          </div>
+        </form>
+      </div>
+    </Wrapper>
   );
 };
 export default Login;
